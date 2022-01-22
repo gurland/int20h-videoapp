8d501be3bb5f3664e50ef447a2159bcb4fef0486
gurland <stas.gurland@gmail.com>
2022-01-22 03:21:45 +0200

    Add terraform remote state configuration on S3

--- infra/main.tf
+++ infra/main.tf
@@ -3,8 +3,17 @@ provider "aws" {
   profile = "personal"
 }
 
-provider "aws"{
+provider "aws" {
   region = "us-east-1"
   alias = "us"
   profile = "personal"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "videoapp-infra-state"
+    key = "infra/terraform.tfstate"
+    region = "eu-central-1"
+    profile = "personal"
+  }
 }
