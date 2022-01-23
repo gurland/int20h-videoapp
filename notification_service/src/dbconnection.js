07e48e992efbb33d64c1330769662c0853f65958
gurland <stas.gurland@gmail.com>
2022-01-23 21:18:11 +0200

    Add Janus, remove notifications

--- notification_service/src/dbconnection.js
+++
@@ -1,8 +0,0 @@
-const  mongoose  = require("mongoose");
-const { MONGO_URI } = require('./config.js');
-const  connect = mongoose.connect(MONGO_URI,
-{
-      useNewUrlParser: true
-    }
-  );
-module.exports = connect;
